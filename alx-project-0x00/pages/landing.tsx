00ea27c48d5d36e3833f2a9f99c943d984a69f01
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-26 11:37:56 +0300

    added rounded lg and rounded full

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -15,7 +15,8 @@ const Landing: React.FC = () => {
           <div className="flex gap-3">
             <Button title="Small sm" styles="text-sm rounded-sm" />
             <Button title="Small md" styles="text-sm rounded-md" />
-            <Button title="Small lg" styles="text-sm rounded-lg" />
+            <Button title="Small md" styles="text-sm rounded-lg" />
+            <Button title="Small lg" styles="text-sm rounded-full" />
           </div>
         </div>
 
@@ -24,7 +25,8 @@ const Landing: React.FC = () => {
           <div className="flex gap-3">
             <Button title="Medium sm" styles="text-base rounded-sm" />
             <Button title="Medium md" styles="text-base rounded-md" />
-            <Button title="Medium lg" styles="text-base rounded-lg" />
+            <Button title="Medium md" styles="text-base rounded-lg" />
+            <Button title="Medium lg" styles="text-base rounded-full" />
           </div>
         </div>
 
@@ -33,7 +35,8 @@ const Landing: React.FC = () => {
           <div className="flex gap-3">
             <Button title="Large sm" styles="text-lg rounded-sm" />
             <Button title="Large md" styles="text-lg rounded-md" />
-            <Button title="Large lg" styles="text-lg rounded-lg" />
+            <Button title="Large md" styles="text-lg rounded-lg" />
+            <Button title="Large lg" styles="text-lg rounded-full" />
           </div>
         </div>
       </div>
